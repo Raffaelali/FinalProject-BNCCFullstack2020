aa54b4584ca9a42fe99a1c7d4804b71f03d53316
Raff <raff.hizz@gmail.com>
2021-06-27 14:41:30 +0700

    topiks,comments,posts

---
+++ database/migrations/2021_06_27_072527_create_topiks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTopiksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('topiks', function (Blueprint $table) {
+            $table->id();
+            $table->integer('post_id');
+            $table->string('topik_name', 12);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('topik');
+    }
+}
